40d5729ea4c7a69dfc7082aa95abe73e2fe10024
Matthias Küblböck <matthias.kueblboeck@yahoo.de>
2018-05-28 15:20:38 +0200

    jwt

--- server/server.js
+++ server/server.js
@@ -20,13 +20,16 @@
     let available;
     let devices = {};
 
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
     app.use(bodyParser.json());
     app.use(cors());
 
     app.get("/devices/available", getAvailable);
     app.post("/authentication", authenticate);
     app.put("/password", changePassword);
+    app.post("/token", verifyTokenOnStartup);
 
     app.get("/devices", getDevices);
     app.post("/devices", addDevice);
@@ -37,19 +40,17 @@
 
     // TODO Create a WebSocket that clients can connect to
     const expressWs = require('express-ws')(app);
-
-    const WebSocketServer = require('ws').Server,
-        wss = new WebSocketServer({ port: 40510 });
-
-    wss.on('connection', function (ws) {
-        ws.on('message', function (message) {
-            console.log('received: %s', message)
-            const jsn = JSON.parse(message);
-            sendUpdatedValue(jsn.index, jsn.value);
+    
+        const WebSocketServer = require('ws').Server,
+            wss = new WebSocketServer({ port: 40510 });
+    
+        wss.on('connection', function (ws) {
+            ws.on('message', function (message) {
+                console.log('received: %s', message)
+            })
+            ws.send("hello Client, i am the server")
         })
-        ws.send("hello Client, i am the server")
-    })
-
+    
     // TODO Check validity of JWT tokens on requests
 
     /**
@@ -57,12 +58,22 @@
      * @param req The request
      * @param res The response
      */
+
     function getAvailable(req, res) {
-        if (!available) {
-            res.status(500).json({ message: "Devices not loaded" });
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
         } else {
-            res.status(200).json(available);
+            if (!available) {
+                res.status(500).json({
+                    message: "Devices not loaded"
+                });
+            } else {
+                res.status(200).json(available);
+            }
         }
+
     }
 
     /**
@@ -71,7 +82,13 @@
      * @param res The response
      */
     function getDevices(req, res) {
-        res.status(200).json(Object.keys(devices).map(index => devices[index]));
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
+        } else {
+            res.status(200).json(Object.keys(devices).map(index => devices[index]));
+        }
     }
 
     /**
@@ -80,18 +97,28 @@
      * @param res The response
      */
     function addDevice(req, res) {
-        if (devices[req.body.index]) {
-            res.status(415).json({ message: "Device already exists" });
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
         } else {
-            devices[req.body.index] = {
-                index: req.body.index,
-                type: req.body.type,
-                title: req.body.title,
-                position: req.body.position,
-                control: req.body.control,
-                successors: req.body.successors || []
-            };
-            res.status(200).json({ message: "Device added" });
+            if (devices[req.body.index]) {
+                res.status(415).json({
+                    message: "Device already exists"
+                });
+            } else {
+                devices[req.body.index] = {
+                    index: req.body.index,
+                    type: req.body.type,
+                    title: req.body.title,
+                    position: req.body.position,
+                    control: req.body.control,
+                    successors: req.body.successors || []
+                };
+                res.status(200).json({
+                    message: "Device added"
+                });
+            }
         }
     }
 
@@ -101,26 +128,46 @@
      * @param res The response
      */
     function deleteDevice(req, res) {
-        if (!devices[req.params.index]) {
-            res.status(404).json({ message: "Device does not exist" });
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
         } else {
-            delete devices[req.params.index];
-            Object.entries(devices).forEach(([key, device]) => deleteArrayEntry(device.successors, +req.params.index));
-            res.status(200).json({ message: "Device deleted" });
+            if (!devices[req.params.index]) {
+                res.status(404).json({
+                    message: "Device does not exist"
+                });
+            } else {
+                delete devices[req.params.index];
+                Object.entries(devices).forEach(([key, device]) => deleteArrayEntry(device.successors, +req.params.index));
+                res.status(200).json({
+                    message: "Device deleted"
+                });
+            }
         }
-    }
 
+    }
     /**
      * Update a device's position
      * @param req The request
      * @param res The response
      */
     function moveDevice(req, res) {
-        if (!devices[req.params.index]) {
-            res.status(404).json({ message: "Device does not exist" });
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
         } else {
-            devices[req.params.index].position = req.body.position;
-            res.status(200).json({ message: "Device position updated" });
+            if (!devices[req.params.index]) {
+                res.status(404).json({
+                    message: "Device does not exist"
+                });
+            } else {
+                devices[req.params.index].position = req.body.position;
+                res.status(200).json({
+                    message: "Device position updated"
+                });
+            }
         }
     }
 
@@ -130,13 +177,25 @@
      * @param res The response
      */
     function addSuccessor(req, res) {
-        if (!devices[req.params.index]) {
-            res.status(404).json({ message: "Start device does not exist" });
-        } else if (!devices[req.body.index]) {
-            res.status(400).json({ message: "End does not exist" });
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
         } else {
-            devices[req.params.index].successors.push(req.body.index);
-            res.status(200).json({ message: "Arrow added" });
+            if (!devices[req.params.index]) {
+                res.status(404).json({
+                    message: "Start device does not exist"
+                });
+            } else if (!devices[req.body.index]) {
+                res.status(400).json({
+                    message: "End does not exist"
+                });
+            } else {
+                devices[req.params.index].successors.push(req.body.index);
+                res.status(200).json({
+                    message: "Arrow added"
+                });
+            }
         }
     }
 
@@ -146,11 +205,19 @@
      * @param res The response
      */
     function deleteSuccessor(req, res) {
-        const device = devices[req.params.predecessor];
-        if (device) {
-            deleteArrayEntry(device.successors, +req.params.successor);
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
+            });
+        } else {
+            const device = devices[req.params.predecessor];
+            if (device) {
+                deleteArrayEntry(device.successors, +req.params.successor);
+            }
+            res.status(200).json({
+                message: "Arrow deleted"
+            });
         }
-        res.status(200).json({ message: "Arrow deleted" });
     }
 
     function deleteArrayEntry(array, entry) {
@@ -160,24 +227,82 @@
         }
     }
 
+
     /**
      * Authenticate the user specified in the request
      * @param req The request
      * @param res The response
      */
     function authenticate(req, res) {
-        const username = req.body.username, password = req.body.password;
-
+        const username = req.body.username,
+            password = req.body.password;
         if (!user) {
-            res.status(500).json({ message: "User data not loaded" });
+            res.status(500).json({
+                message: "User data not loaded"
+            });
         } else if (!username || !password) {
-            res.status(400).json({ message: "Bad request" });
+            res.status(400).json({
+                message: "Bad request"
+            });
         } else if (username !== user.username || password !== user.password) {
-            res.status(401).json({ message: "Bad credentials", errors: { credentials: true } });
+            res.status(401).json({
+                message: "Bad credentials",
+                errors: {
+                    credentials: true
+                }
+            });
         } else {
-            // TODO Send a JWT back to the client
-            res.status(200).json({ message: "Successfully logged in" });
+            var token = generateToken(username);
+            res.status(200).json({
+                message: "Successfully logged in",
+                token: token
+            })
         }
+
+    }
+
+    function verifyTokenOnStartup(req, res) {
+        const token = req.body.token;
+        console.log(token);
+
+        if (token) {
+            var decoded = jwt.decode(token);
+            if (Date.now() / 1000 > decoded.exp) {
+                res.send(JSON.stringify('Token Expired'));
+            } else {
+                res.send(JSON.stringify('Token Valid'));
+            }
+
+        } else {
+            res.status(500).json({
+                message: "No Token provided"
+            });
+        }
+    }
+
+    function validateToken(token) {
+        var decoded = jwt.verify(token, 'secret');
+        console.log(decoded);
+        return decoded != null ? true : false;
+    }
+
+    function getTokenFromHeader(req) {
+        const authorization = req.headers.authorization;
+        const length = authorization.length;
+        return authorization.slice(8, length - 1);
+    }
+
+    function checkBearerToken(req) {
+        var token = getTokenFromHeader(req);
+        return validateToken(token);
+    }
+
+
+    function generateToken(username) {
+        return jwt.sign({
+            exp: Math.floor(Date.now() / 1000 + (60 * 60)), // 1 hour
+            data: username
+        }, 'secret');
     }
 
     /**
@@ -186,25 +311,45 @@
      * @param res The response
      */
     function changePassword(req, res) {
-        const oldPassword = req.body.oldPassword, newPassword = req.body.newPassword;
-
-        if (!user) {
-            res.status(500).json({ message: "User data not loaded" });
-        } else if (!oldPassword || !newPassword) {
-            res.status(400).json({ message: "Bad request" });
-        } else if (oldPassword !== user.password) {
-            res.status(400).json({ message: "Old password wrong", errors: { oldPassword: true } });
-        } else {
-            const data = `username: ${user.username}\npassword: ${newPassword}`;
-            fs.writeFile("./resources/login.config", data, {}, function (err) {
-                if (err) {
-                    console.log("Error writing user config: ", err);
-                    res.status(500).json({ message: "Password could not be stored" });
-                } else {
-                    user.password = newPassword;
-                    res.status(200).json({ message: "Password successfully updated" });
-                }
+        if (!checkBearerToken(req)) {
+            res.status(500).json({
+                message: "Token is invalid"
             });
+        } else {
+            const oldPassword = req.body.oldPassword,
+                newPassword = req.body.newPassword;
+
+            if (!user) {
+                res.status(500).json({
+                    message: "User data not loaded"
+                });
+            } else if (!oldPassword || !newPassword) {
+                res.status(400).json({
+                    message: "Bad request"
+                });
+            } else if (oldPassword !== user.password) {
+                res.status(400).json({
+                    message: "Old password wrong",
+                    errors: {
+                        oldPassword: true
+                    }
+                });
+            } else {
+                const data = `username: ${user.username}\npassword: ${newPassword}`;
+                fs.writeFile("./resources/login.config", data, {}, function (err) {
+                    if (err) {
+                        console.log("Error writing user config: ", err);
+                        res.status(500).json({
+                            message: "Password could not be stored"
+                        });
+                    } else {
+                        user.password = newPassword;
+                        res.status(200).json({
+                            message: "Password successfully updated"
+                        });
+                    }
+                });
+            }
         }
     }
 
@@ -215,13 +360,6 @@
      */
     function sendUpdatedValue(index, value) {
         // TODO Send the data to connected WebSocket clients
-        console.log("jetzt update: " + index + " " + value);
-        wss.on('connection', function (ws) {
-            ws.send(index, value)
-            console.log("client gschickt");
-        })
-        // ws.send("asdfasdf");
-        
     }
 
     /**
